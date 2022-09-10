316ec7d2748fcfa16ce1cbdce2c389d962caf757
LexPrimen <105244473+LexPrimen@users.noreply.github.com>
2022-09-09 23:10:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-
+const mysql = require('mysql2');
 const express = require('express');
+require('dotenv').config();
 const path = require('path');
 const hbs = require('hbs');
 const app = express();
-const PORT = 3000 || 8080;
+const PORT = process.env.PORT || 8080;
 
 
-// const conexion = mysql.createConnection({
-//     host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE   
-// });
+
+const conexion = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE   
+});
 
 
-// console.log();
 
 
-//    conexion.connect((err) => {
-//     if (err) {
-//         console.error(`error de conexion: ${err.stack}`)
-//         return;
-//     }
-//     console.log(`conectado a la base de datos ${process.env.DATABASE}`);
-// });
+
+console.log();
+
+
+   conexion.connect((err) => {
+    if (err) {
+        console.error(`error de conexion: ${err.stack}`)
+        return;
+    }
+    console.log(`conectado a la base de datos ${process.env.DATABASE}`);
+});
+
 
 
 
@@ -38,7 +44,6 @@ hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 app.get('/', (req, res, next) =>{
     res.render('index', {
-        titulo:'Bienvenidos a Nuestra Pagina ',
         style:'styles.css'
     })
 });
@@ -57,9 +62,55 @@ app.get('/page', (req,res) =>{
 
 
 
+app.listen(PORT, () => {
+    console.log(`el servidor esta funcionando en el puerto ${PORT}`)
+ });
 
 
-//// EJEMPLO POST Y GET ETC////
+
+
+
+ 
+ app.post('/page' , (req,res) => {
+    
+    
+   
+     const {producto,precio} = req.body; 
+        
+        console.log(producto, precio);
+     
+         if (producto =='' || precio == ''){
+            let validacion = 'rellenar los campos necesarios..';
+    
+            res.render('page', {
+                titulo: 'formulario para productos',
+                validacion
+            });
+         } else{
+            res.render('validacion', {
+                titulo: 'formulario enviado',
+                style:'styles.css',
+          }) ;
+         }
+    
+        let datos = {
+            producto: producto,     //La primera palabra es el nombre de la base de datos y el siguiente es el valor que tiene en el programa//
+            precio: precio
+        }
+         
+          let sql ='INSERT INTO trabajopp set ?';
+    
+          conexion.query(sql, datos, (err, result) => {
+            if (err) throw err;
+            return
+          });
+ 
+        res.send(`Datos recibidos: producto: ${producto} precio: ${precio}`); 
+        
+ });
+     
+
+        
 
 
 
@@ -135,4 +186,4 @@ app.get('/page', (req,res) =>{
 //         res.render('formulario',{
 //             titulo: 'formulario para productos'
 //         });
-//       });
+//       })
